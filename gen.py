7dba7c8acbec91647de2ae448815591824b02515
Lana Black <lana@illuminati.industries>
2023-05-30 07:45:44 +0000

    Regenerate json without duplicate keys.

--- gen.py
+++ gen.py
@@ -5,11 +5,18 @@ import random
 
 N=1000000
 
+def rand_str():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=5))
+
 with open("file.json", "w") as f:
     f.write("{\n")
+    s = set()
     for i in range(N):
-        k = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
-        v = ''.join(random.choices(string.ascii_lowercase, k=5))
+        k = rand_str()
+        while k in s:
+            k = rand_str()
+        s.add(k)
+        v = rand_str()
         f.write("  \"{0}\": \"{1}\"".format(k,v))
         if i == N-1:
             f.write("\n")
